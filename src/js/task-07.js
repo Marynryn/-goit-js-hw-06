2e0980ecebd0c39521cea53feae95f1d6b22e99a
Marynryn <murmar95@gmail.com>
2023-09-04 19:13:47 +0300

    ввв

--- src/js/task-07.js
+++
@@ -1,5 +0,0 @@
-const input = document.getElementById("font-size-control");
-const text = document.getElementById("text")
-input.addEventListener("input", handleInput);
-function handleInput (event) {
-   text.style.fontSize = event.currentTarget.value + "px"}
